08cad13d62cfc82eee076b118ef6533a9778a925
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-04-20 14:50:49 +0300

    [Person Model] add_person method added

--- Models/person.py
+++ Models/person.py
@@ -3,7 +3,14 @@
 
 
 class Person(object):
-    def __init__(self, name, person_id, role):  # Default constructor.
+    # Default constructor.
+    def __init__(self, name, person_id, role, wants_accomodation):
         self.name = name
         self.id = id
         self.role = role
+        self.wants_accomodation = wants_accomodation
+
+    def add_person():
+        new_person = ((input("Enter Firstname Secondname role wants_accomodation separated by space  ")).split())
+
+        return new_person
