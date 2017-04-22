b81c3700354eea0eb83a9e3baad152d313eef776
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-04-22 21:28:39 +0300

    [model_tests]add_person_test added

--- Tests/test_person.py
+++ Tests/test_person.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 from Models.person import Person, Fellow, Staff
 
 
@@ -13,6 +13,14 @@ class FellowTest(TestCase):
     # check that class Fellow is an instance/subclass of class Person
     def test_fellow_is_instance_of_person(self):
         self.assertTrue(self.fellow, Person)
+
+    def test_add_person(self):
+        # Test user can input Fellow details
+        # mocked one object because am calling this function once here.
+        fake_input = mock.Mock(side_effect=["OLU JACOBS FELLOW Y", ""])
+        with mock.patch("builtins.input", fake_input):
+            new_person = Fellow.add_person()
+        self.assertEqual(new_person, "OLU JACOBS FELLOW Y")
 
 
 if __name__ == "__main__":
