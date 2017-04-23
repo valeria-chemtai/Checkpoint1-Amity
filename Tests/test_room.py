0f849c6a121fd42c6682ac9921018eb712d84be6
valeria-chemtai <valeriachemtai28@gmail.com>
2017-04-23 19:10:06 +0300

    [model_tests]test_create_room added (#17)
    updated model tests

--- Tests/test_room.py
+++ Tests/test_room.py
@@ -8,6 +8,16 @@ class TestRoom(TestCase):
     def setUp(self):  # setUp so that Room() will be called as self.room
         self.room = Room()
 
-    def tearDown(self):  # tearDown to delete self.room after every
+    def tearDown(self):  # tearDown to delete self.room after every call
         del self.room
 
+    def test_create_room(self):  # Test method to create new rooms
+        # Mocking user inputs
+        fake_input = mock.Mock(side_effect=["room1", "room2", "room3", ""])
+        with mock.patch("builtins.input", fake_input):
+            new_room = Room.create_room(self)
+        self.assertEqual(new_room, ["room1", "room2", "room3", ""])
+
+
+if __name__ == "__main__":
+    unittest.main()
