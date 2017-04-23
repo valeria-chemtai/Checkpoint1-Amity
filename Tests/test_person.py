e5efbc232b59b55555e069d4ed64a0139e64081e
valeria-chemtai <valeriachemtai28@gmail.com>
2017-04-23 20:44:52 +0300

    Develop (#23)
    * Database model with Andelans and Amity Tables
    
    * Database model with Andelans Table
    
    * Database model with Amity Table added
    
    * [model_tests] add_person_test
    
    * [model_tests]class Fellow Tests
    
    * [model_tests]add_person_test added
    
    * [model_tests]getting started with StaffTests
    
    * [model_tests]test_add_person in StaffTest
    
    * [model_tests]setUp and tearDown methods added
    
    * [model_tests]test_create_room added
    
    * [model_tests]test_create_room added (#17)
    
    updated model tests
    
    * [room_model]__init__ and create_room methods added (#18)
    
    updated model
    
    * Delete controller.py
    
    will use amity.py
    
    * Delete .DS_Store
    
    gitignore
    
    * Delete .DS_Store
    
    gitignore
    
    * Delete controller.py
    
    use amity.py
    
    * Delete .DS_Store
    
    gitignore
    
    * Delete .DS_Store
    
    gitignore
    
    * Delete controller.py
    
    amity.py

---
+++ Tests/test_person.py
@@ -0,0 +1,49 @@
+from unittest import TestCase, mock
+from Models.person import Person, Fellow, Staff
+
+
+class FellowTest(TestCase):
+    def setUp(self):
+        # call Fellow(Person) as self.fellow before running tests in this class
+        self.fellow = Fellow()
+
+    def tearDown(self):
+        del self.fellow  # after every test delete self.fellow
+
+    # check that class Fellow is an instance/subclass of class Person
+    def test_fellow_is_instance_of_person(self):
+        self.assertTrue(self.fellow, Person)
+
+    def test_add_person(self):
+        # Test user can input Fellow details
+        # mocked one object because am calling this function once here.
+        fake_input = mock.Mock(side_effect=["OLU JACOBS FELLOW Y", ""])
+        with mock.patch("builtins.input", fake_input):
+            new_fellow = Fellow.add_person()
+        self.assertEqual(new_fellow, "OLU JACOBS FELLOW Y")
+
+
+class StaffTest(TestCase):
+    def setUp(self):
+        # call class Staff as self.staff
+        self.staff = Staff()
+
+    def teardown(self):
+        del self.staff
+
+    def test_staff_is_instance_of_person(self):
+        # check if class staff is a subclass of Person
+        self.assertTrue(self.staff, Person)
+
+
+    def test_add_person(self):
+        # Test user can input staff details i.e name and role
+        # mocking inputs
+        fake_input = mock.Mock(side_effect=["DOMINIC WALTERS STAFF", ""])
+        with mock.patch("builtins.input", fake_input):
+            new_staff = Staff.add_person(self)
+        self.assertEqual(new_staff, "DOMINIC WALTERS STAFF")
+
+
+if __name__ == "__main__":
+    unittest.main()
