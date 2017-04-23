fc7ba51fc7d659ceeccb0706b87ebfabddf38434
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-04-23 21:06:35 +0300

    [room_model]create room function added

---
+++ Models/room.py
@@ -0,0 +1,33 @@
+"""Room model"""
+
+# class Room
+
+
+class Room(object):
+    def __init__(self, name=None, room_id=None):
+        self.name = name
+        self.room_id = room_id
+
+    def create_room(self):
+        # prompt for room names
+        rooms_available = []
+        while True:
+            new_room = input("Room {}'s name: ". format(
+                len(rooms_available) + 1))
+            if not new_room:
+                break
+            rooms_available.append(new_room)
+        return rooms_available
+
+
+# class office a subclass of Room
+
+
+class Office(Room):
+    pass
+
+# class LivingSpace a subclass of Room
+
+
+class LivingSpace(Room):
+    pass
