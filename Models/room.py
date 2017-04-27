bb2a0b20ca182684cc079702aebc9076a21f54a0
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-04-28 01:00:45 +0300

    [room_model]default constructor properly defined

--- Models/room.py
+++ Models/room.py
@@ -4,32 +4,38 @@
 
 
 class Room(object):
-    def __init__(self, name=None, room_id=None):
+    def __init__(self, name=" ", room_id=None,
+                 purpose=None, occupants=[]):
         self.name = name
         self.room_id = room_id
-
-    def create_room(self):
-        # prompt for room names
-        rooms_available = []
-        while True:
-            new_room = input("Room {}'s name: ". format(
-                len(rooms_available) + 1))
-            if not new_room:
-                break
-            rooms_available.append(new_room)
-        return rooms_available
+        self.purpose =purpose
+        self.occupants = occupants
 
 
 # class office a subclass of Room
 
 
 class Office(Room):
-    def __init__(self, name=None, room_id=None):
-        Room.__init__(self, name, room_id)
+    def __init__(self, name=" ", room_id=None,
+                 purpose=None, occupants=[], max_capacity=6):
+        Room.__init__(self, name, room_id, purpose, occupants)
+        self.name = name
+        self.room_id = room_id
+        self.purpose =purpose
+        self.occupants = occupants
+        self.max_capacity = 6
+        
 
 # class LivingSpace a subclass of Room
 
 
 class LivingSpace(Room):
-    def __init__(self, name=None, room_id=None):
-        Room.__init__(self, name, room_id)
+    def __init__(self, name=" ", room_id=None,
+                 purpose=None, occupants=[], max_capacity=4):
+        Room.__init__(self, name, room_id, purpose, occupants)
+        self.name = name
+        self.room_id = room_id
+        self.occupants = occupants
+        self.purpose = purpose
+        self.max_capacity = 4
+
