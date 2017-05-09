19efbcba69fc25ac220e2fa8bb79351c84566b46
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-05-09 06:09:44 +0300

    [Amity]Reallocate functionality added with passing tests

--- Tests/test_amity.py
+++ Tests/test_amity.py
@@ -13,68 +13,77 @@ from Models.room import Room, Office, LivingSpace
 
 
 class TestAmity(TestCase):
-    def setUp(self):
-        # call this function as specified for each test
-        self.amity = Amity()
+  def setUp(self):
+    # call this function as specified for each test
+    self.amity = Amity()
 
-    def tearDown(self):
-        del self.amity
+  def tearDown(self):
+    del self.amity
 
-    def test_create_room(self):
-        """Test room is succesfully created"""
+  def test_create_room(self):
+    """Test room is succesfully created"""
 
-        self.assertEqual(self.amity.create_room("Accra", "OFFICE"),
-                         "Room Created")
-        self.assertEqual(self.amity.create_room("Unono", "LIVINGSPACE"),
-                         "Room Created")
+    self.assertEqual(self.amity.create_room("Accra", "OFFICE"),
+                     "Room Created")
+    self.assertEqual(self.amity.create_room("Unono", "LIVINGSPACE"),
+                     "Room Created")
 
-    def test_room_only_created_once(self):
-        """Test room is only created once"""
-        self.amity.create_room("Accra", "OFFICE")
-        self.assertEqual(self.amity.create_room("Accra", "OFFICE"),
-                         "{} Exists in Amity.".format("Accra"))
+  def test_room_only_created_once(self):
+    """Test room is only created once"""
+    self.amity.create_room("Accra", "OFFICE")
+    self.assertEqual(self.amity.create_room("Accra", "OFFICE"),
+                     "{} Exists in Amity.".format("Accra"))
 
-    def test_valid_room_names(self):
-        """Test valid room name"""
-        self.assertEqual(self.amity.create_room(
-            123, "OFFICE"), "Invalid Room Name")
+  def test_valid_room_names(self):
+    """Test valid room name"""
+    self.assertEqual(self.amity.create_room(
+        123, "OFFICE"), "Invalid Room Name")
 
-    def test_add_person(self):
-        """Test a person/people are succesfully added"""
-        self.amity.create_room("Accra", "OFFICE")
-        self.amity.create_room("Unono", "LIVINGSPACE")
-        self.assertEqual(self.amity.add_person("Dominic", "Walters", "STAFF",
-                                               "N"), "Staff Added")
-        self.assertEqual(self.amity.add_person("Oluwafemi", "Sule", "FELLOW",
-                                               "N"), "Fellow Added")
-        self.assertEqual(self.amity.add_person("Simon", "Patterson", "FELLOW",
-                                               "Y"),
-                         "Fellow Added and LIvingSpace Allocated")
+  def test_add_person(self):
+    """Test a person/people are succesfully added"""
+    self.amity.create_room("Accra", "OFFICE")
+    self.amity.create_room("Unono", "LIVINGSPACE")
+    self.assertEqual(self.amity.add_person("Dominic", "Walters", "STAFF",
+                                           "N"), "Staff Added")
+    self.assertEqual(self.amity.add_person("Oluwafemi", "Sule", "FELLOW",
+                                           "N"), "Fellow Added")
+    self.assertEqual(self.amity.add_person("Simon", "Patterson", "FELLOW",
+                                           "Y"),
+                     "Fellow Added and LIvingSpace Allocated")
 
-    def test_add_existing_person(self):
-        """Test person is added once"""
-        self.amity.add_person("Angie", "Sule", "STAFF", "N")
-        self.assertEqual(self.amity.add_person("Angie", "Sule", "STAFF", "N"),
-                         "{} Exists in Amity.".format("Angie Sule"))
+  def test_add_existing_person(self):
+    """Test person is added once"""
+    self.amity.add_person("Angie", "Sule", "STAFF", "N")
+    self.assertEqual(self.amity.add_person("Angie", "Sule", "STAFF", "N"),
+                     "{} Exists in Amity.".format("Angie Sule"))
 
-    def test_valid_person_name(self):
-        """Test valid person name"""
-        self.assertEqual(self.amity.add_person(123, 123, "STAFF", "N"),
-                         "Invalid Person Name")
+  def test_valid_person_name(self):
+    """Test valid person name"""
+    self.assertEqual(self.amity.add_person(123, 123, "STAFF", "N"),
+                     "Invalid Person Name")
 
-    def test_reallocate_person_not_in_amity(self):
-        """Test reallocate a person not in Amity yet"""
-        self.amity.create_room("Accra", "OFFICE")
-        self.assertEqual(self.amity.reallocate_person("Rose", "Wambui",
-                                                      "Accra"),
-                         "Add {} to Amity first".format("Rose Wambui"))
+  def test_reallocate_person_not_in_amity(self):
+    """Test reallocate a person not in Amity yet"""
+    self.amity.create_room("Accra", "OFFICE")
+    self.assertEqual(self.amity.reallocate_person("Rose", "Wambui",
+                                                  "Accra"),
+                     "Add {} to Amity first".format("Rose Wambui"))
 
-    def test_cannot_reallocate_person_to_unavailable_room(self):
-        """Test person can not be reallocated to unavailable room """
-        self.amity.create_room("Accra", "OFFICE")
-        self.amity.add_person("Rose", "Wambui", "STAFF", "N")
-        self.assertEqual(self.amity.reallocate_person("Rose", "Wambui", "Cairo"), "{} is not a room in Amity".format("Cairo"))
+  def test_cannot_reallocate_person_to_unavailable_room(self):
+    """Test person can not be reallocated to unavailable room """
+    self.amity.create_room("Accra", "OFFICE")
+    self.amity.add_person("Rose", "Wambui", "STAFF", "N")
+    self.assertEqual(self.amity.reallocate_person("Rose", "Wambui", "Cairo"),
+                     "{} is not a room in Amity".format("Cairo"))
+
+  def test_successful_reallocation(self):
+    """Test if person is successfully reallocated"""
+    self.amity.create_room("Accra", "OFFICE")
+    self.amity.add_person("Rose", "Wambui", "STAFF", "N")
+    self.amity.create_room("Cairo", "OFFICE")
+    self.assertEqual(self.amity.reallocate_person(
+        "Rose", "Wambui", "Cairo"), "Reallocated Successfully")
 
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
