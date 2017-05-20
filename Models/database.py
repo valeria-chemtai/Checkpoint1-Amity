61db20cada63f995ffd7812016c272f14ba0a775
Valeria Chemtai <valeriachemtai28@gmail.com>
2017-05-20 22:50:10 +0300

    [models]Remove unnecessary doc strings

--- Models/database.py
+++ Models/database.py
@@ -5,17 +5,12 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-""" create table called Andelans"""
-
 
 class People(Base):
     __tablename__ = "Andelans"
     Name = Column(String(250), primary_key=True, nullable=False)
     Role = Column(String(250))
     RoomAllocated = Column(String(250), nullable=True)  # Room allocated
-
-
-"""create table called Amity"""
 
 
 class Rooms(Base):
@@ -25,7 +20,7 @@ class Rooms(Base):
     Occupants = Column(String(250), nullable=True)
 
 
-""" Create engine that stores data in local directory"""
+# Create engine that stores data in local directory"""
 engine = create_engine("sqlite:///Amity_database.db")
 
 Base.metadata.create_all(engine)  # create tables in the engine
